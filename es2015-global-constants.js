a42a8d0de29aeef0382a6e64b6b483098c6a75de
alexm <alex-m@outlook.com>
2022-10-13 12:31:44 -0700

    Added my code responses

--- es2015-global-constants.js
+++ es2015-global-constants.js
@@ -2,3 +2,10 @@
 // 10/13/22
 // Exercise 10.2.7: let and const
 
+// ES5 version
+var VARPI = 3.14;
+VARPI = 42;
+
+// ES2015 version
+const PI = 3.14;
+PI = 42; // Will throw TypeError: Assignment to constant variable
